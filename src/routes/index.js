7b2ac735a8a58cd8d8e01b835dc9b4bc627bc370
RaulSoares01 <24006923@uepg.br>
2026-08-10 13:59:54 -0300

    geektopia routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authRoutes = require('./authRoutes');
+const geektopiaRoutes = require('./geektopiaRoutes');
 
 // Agrupa todas as rotas sob o prefixo correto
 router.use('/auth', authRoutes);
+router.use('/geektopia', geektopiaRoutes);
 
 module.exports = router;
